f84c84619c73339f9828c4df4ef3ebe9eb66bd6d
gabrielcliseru <settlersxp@gmail.com>
2025-03-06 21:13:16 +0100

    working cors and server side

--- frontend/src/routes/prop_firms/list/+page.svelte
+++ frontend/src/routes/prop_firms/list/+page.svelte
@@ -10,7 +10,12 @@
     let prop_firms = $state<PropFirm[]>([]);
 
     $effect(() => {
-        fetch("http://100.66.179.48:3100/prop_firms/")
+        fetch("/api/prop_firms", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
             .then((res) => res.json())
             .then((data) => (prop_firms = data as PropFirm[]));
     });
@@ -18,6 +23,9 @@
 
 <div>
     <h1>Prop Firms</h1>
+    <a href="/prop_firms/" class="bg-blue-500 text-white px-4 py-2 rounded-md">
+        New
+    </a>
     <ul>
         {#each prop_firms as prop_firm}
             <li>{prop_firm.name}</li>
